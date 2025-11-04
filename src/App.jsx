eae0285388b612da783bf8fd3efcb47c97cb3129
anasadnan2 <anasadnanabojaber@gmail.com>
2025-11-04 02:56:38 +0200

    Initial project setup with dependencies and dev tools

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 function App() {
-  return <>anas</>;
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-zinc-900 text-4xl text-emerald-400 font-bold">
+      Tailwind is working ✅
+    </div>
+  );
 }
 
 export default App;
